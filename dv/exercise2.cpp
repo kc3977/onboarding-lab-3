045f4960e60d0d6ee5961d2a3c08e33aa430b552
kc3977 <102318279+kc3977@users.noreply.github.com>
2023-02-04 17:37:44 -0500

    Update exercise2.cpp

--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -2,4 +2,27 @@
 
 int main() {
   VExercise2 model;
+  bool wrong = false;
+  model.init = 0b0000000000000000; // initial settings
+  model.reset = 1;
+  model.eval();
+  model.clk = 1;
+  model.eval();
+  model.clk = 0;
+  model.eval();
+  if (model.out != 0b1111111111111111) wrong = true; // check if changed to all ones
+  model.reset = 0;
+  model.eval();
+  model.clk = 1;
+  model.eval();
+  model.clk = 0;
+  model.eval();
+  if (model.out != (0b1111111111111110 | (1 ^ 1 ^ 1 ^ 1))) wrong = true; // check if changed properly
+  model.clk = 1;
+  model.eval();
+  model.clk = 0;
+  model.eval();
+  if (model.out != (0b1111111111111110 | (1 ^ 1 ^ 1 ^ 1))<<1 | (1 ^ 1 ^ 1 ^ (1 ^ 1 ^ 1 ^ 1)) ) wrong = true;
+  // looks ugly but check if changed properly again
+  if (wrong) return 1;
 }
